52809e0c2c7b9b3cef530da20cf9916a0f69a924
nanzhudebao <3113872084@qq.com>
2024-01-06 15:34:17 +0800

    第一次

---
+++ untitled/src/main/java/org/example/Main.java
@@ -0,0 +1,12 @@
+package org.example;
+
+/**
+ * @author ngr
+ * @version 1.0
+ * @date 2024/1/6 15:33
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
